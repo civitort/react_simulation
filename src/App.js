a3b902a26d87b3fcae458903ef13c12f63bf9fd0
mishima1500 <k.mishima@fukuyamaconsul.co.jp>
2023-01-26 16:45:11 +0900

    グラフの動的描画

--- src/App.js
+++ src/App.js
@@ -20,79 +20,7 @@ import HeaderLayout from "./components/layouts/HeaderLayout";
 import { theme } from "./styles/Theme";
 import axios from "axios";
 import Chart from "react-google-charts";
-const graph_data = [
-  ["時間", "交通量", "推計補正", "交通容量1", "交通容量2", "渋滞長"],
-  ["01-01-00", 306, 65, 1200, 1500, 0],
-  ["01-01-01", 311, 66, 1200, 1500, 0],
-  ["01-01-02", 257, 55, 1200, 1500, 0],
-  ["01-01-03", 264, 56, 1200, 1500, 0],
-  ["01-01-04", 269, 57, 1200, 1500, 0],
-  ["01-01-05", 260, 55, 1200, 1500, 0],
-  ["01-01-06", 230, 49, 1200, 1500, 0],
-  ["01-01-07", 354, 75, 1200, 1500, 0],
-  ["01-01-08", 538, 114, 1200, 1500, 0],
-  ["01-01-09", 637, 135, 1200, 1500, 0],
-  ["01-01-10", 845, 179, 1200, 1500, 0],
-  ["01-01-11", 1010, 214, 1200, 1500, 0],
-  ["01-01-12", 1001, 212, 1200, 1500, 0],
-  ["01-01-13", 1097, 233, 1200, 1500, 0],
-  ["01-01-14", 1207, 256, 1200, 1500, 0],
-  ["01-01-15", 1430, 303, 1200, 1500, 0],
-  ["01-01-16", 1470, 312, 1200, 1500, 0],
-  ["01-01-17", 1331, 283, 1200, 1500, 0],
-  ["01-01-18", 945, 201, 1200, 1500, 0],
-  ["01-01-19", 731, 155, 1200, 1500, 0],
-  ["01-01-20", 683, 145, 1200, 1500, 0],
-  ["01-01-21", 592, 126, 1200, 1500, 0],
-  ["01-01-22", 483, 103, 1200, 1500, 0],
-  ["01-01-23", 403, 86, 1200, 1500, 0],
-  ["01-02-00", 355, 75, 1200, 1500, 0],
-  ["01-02-01", 280, 60, 1200, 1500, 0],
-  ["01-02-02", 260, 55, 1200, 1500, 0],
-  ["01-02-03", 204, 43, 1200, 1500, 0],
-  ["01-02-04", 224, 48, 1200, 1500, 0],
-  ["01-02-05", 220, 47, 1200, 1500, 0],
-  ["01-02-06", 259, 55, 1200, 1500, 0],
-  ["01-02-07", 469, 100, 1200, 1500, 0],
-  ["01-02-08", 919, 195, 1200, 1500, 0],
-  ["01-02-09", 1359, 288, 1200, 1500, 0],
-  ["01-02-10", 1540, 327, 1200, 1500, 200],
-  ["01-02-11", 1758, 373, 1200, 1500, 2700],
-  ["01-02-12", 1638, 348, 1200, 1500, 4600],
-  ["01-02-13", 1647, 350, 1200, 1500, 6600],
-  ["01-02-14", 1879, 399, 1200, 1500, 9650],
-  ["01-02-15", 1868, 397, 1200, 1500, 12700],
-  ["01-02-16", 1699, 361, 1200, 1500, 14900],
-  ["01-02-17", 1420, 301, 1200, 1500, 15800],
-  ["01-02-18", 1690, 359, 1200, 1500, 17950],
-  ["01-02-19", 1215, 258, 1200, 1500, 17850],
-  ["01-02-20", 1034, 219, 1200, 1500, 16850],
-  ["01-02-21", 961, 204, 1200, 1500, 15450],
-  ["01-02-22", 987, 210, 1200, 1500, 14250],
-  ["01-02-23", 928, 197, 1200, 1500, 12750],
-  ["01-03-00", 769, 163, 1200, 1500, 10450],
-  ["01-03-01", 657, 140, 1200, 1500, 7600],
-  ["01-03-02", 508, 108, 1200, 1500, 4100],
-  ["01-03-03", 436, 92, 1200, 1500, 200],
-  ["01-03-04", 342, 73, 1200, 1500, 0],
-  ["01-03-05", 295, 63, 1200, 1500, 0],
-  ["01-03-06", 356, 76, 1200, 1500, 0],
-  ["01-03-07", 490, 104, 1200, 1500, 0],
-  ["01-03-08", 891, 189, 1200, 1500, 0],
-  ["01-03-09", 1286, 273, 1200, 1500, 0],
-  ["01-03-10", 1623, 344, 1200, 1500, 600],
-  ["01-03-11", 1813, 385, 1200, 1500, 3400],
-  ["01-03-12", 1687, 358, 1200, 1500, 5550],
-  ["01-03-13", 1777, 377, 1200, 1500, 8150],
-  ["01-03-14", 1934, 410, 1200, 1500, 11500],
-  ["01-03-15", 966, 205, 1200, 1500, 10150],
-  ["01-03-16", 804, 171, 1200, 1500, 8050],
-  ["01-03-17", 1464, 311, 1200, 1500, 9100],
-  ["01-03-18", 1705, 362, 1200, 1500, 11350],
-  ["01-03-19", 1355, 287, 1200, 1500, 11900],
-  ["01-03-20", 1006, 214, 1200, 1500, 10750],
-  ["01-03-21", 1029, 218, 1200, 1500, 9750],
-];
+
 const graph_options = {
   title: "TEST データ A ～ B区間　上り方向",
   titleTextStyle: {
@@ -191,8 +119,9 @@ const initialState = {
   },
   view_data: {},
   graph_option: {
-    day: ""
-  }
+    day: "2021-01-01",
+  },
+  graph_data: {},
 };
 const cols = {
   days: 0,
@@ -208,6 +137,13 @@ const cols = {
   carlen: 10,
   suikei: 11,
 };
+
+const getStringMMDD = (obj) => {
+  return `${String(obj.getMonth() + 1).padStart(2, "0")}${String(
+    obj.getDate()
+  ).padStart(2, "0")}`;
+};
+
 function App() {
   // データを1時間ピッチで集計する
   const calcHourData = (res, state) => {
@@ -263,6 +199,77 @@ function App() {
     return result;
   };
 
+  const calcViewData = (state) => {
+    // 指定日付の取得
+    var setDay = state.graph_option.day;
+    // 年、月、日の取得
+    var year = setDay.substring(0, 4);
+    var month = setDay.substring(5, 7);
+    var day = setDay.substring(8, 10);
+    console.log(setDay);
+    var dayObject = new Date(year, month - 1, day);
+    // 年始、年末は特殊処理
+    if ((month === "01") & (day === "01")) {
+      var day1 = getStringMMDD(new Date(dayObject));
+      var day2 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() + 1))
+      );
+      var day3 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() + 2))
+      );
+    } else if ((month === "12") & (day === "31")) {
+      var day1 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() - 2))
+      );
+      var day2 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() - 1))
+      );
+      var day3 = getStringMMDD(new Date(dayObject));
+    } else {
+      var day1 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() - 1))
+      );
+      var day2 = getStringMMDD(dayObject);
+      var day3 = getStringMMDD(
+        new Date(new Date(dayObject).setDate(dayObject.getDate() + 1))
+      );
+    }
+    var setDays = [day1, day2, day3];
+
+    // 1時間データをフィルタする
+    const filterdata = state.view_data.filter((item) =>
+      setDays.includes(item[cols.days].substring(4, 8))
+    );
+    const result = filterdata.map((item) => {
+      var time =
+        item[cols.days].substring(4, 8) +
+        "-" +
+        item[cols.hour]
+          .substring(0, item[cols.hour].length - 1)
+          .padStart(2, "0");
+      return [
+        time,
+        item[cols.all],
+        item[cols.suikei],
+        state.calc_option.capacity_normal,
+        state.calc_option.capacity_jutai,
+        item[cols.jutai],
+      ];
+    });
+    result.sort(function (a, b) {
+      return Number(a[0]) - Number(b[0]);
+    });
+    result.unshift([
+      "時間",
+      "交通量",
+      "推計補正",
+      "交通容量1",
+      "交通容量2",
+      "渋滞長",
+    ]);
+    return result;
+  };
+
   // 描画用データを作る
 
   const [reading, setReading] = useState(false);
@@ -279,10 +286,16 @@ function App() {
           },
         };
         if (apiData !== undefined) {
+          // 1時間データ、グラフデータを更新
           var dat = calcHourData(apiData, result);
           result = {
             ...result,
             view_data: dat,
+          };
+          var graph = calcViewData(result);
+          result = {
+            ...result,
+            graph_data: graph,
           };
         }
         return result;
@@ -296,27 +309,48 @@ function App() {
           },
         };
         if (apiData !== undefined) {
+          // 1時間データ、グラフデータを更新
           var dat = calcHourData(apiData, result);
           result = {
             ...result,
             view_data: dat,
           };
+          var graph = calcViewData(result);
+          result = {
+            ...result,
+            graph_data: graph,
+          };
         }
         return result;
       }
       case "reset_data":
-        var dat = calcHourData(apiData, state);
-        return {
-          ...state,
+        // 1時間データ、グラフデータを更新
+        var result = { ...state };
+        var dat = calcHourData(apiData, result);
+        result = {
+          ...result,
           view_data: dat,
         };
+        var graph = calcViewData(result);
+        result = {
+          ...result,
+          graph_data: graph,
+        };
+        return result;
       case "change_graph_option":
-        return {
+        // グラフデータを更新
+        var result = {
           ...state,
           graph_option: {
             [action.field]: action.payload,
-          }
-        }
+          },
+        };
+        var graph = calcViewData(result);
+        result = {
+          ...result,
+          graph_data: graph,
+        };
+        return result;
       default:
         throw new Error();
     }
@@ -352,7 +386,7 @@ function App() {
                 <>
                   <Select
                     maxW={"200px"}
-                    placeholder='データ選択'
+                    placeholder="データ選択"
                     onChange={(e) =>
                       dispatch({
                         type: "change_data_option",
@@ -360,7 +394,6 @@ function App() {
                         payload: e.currentTarget.value,
                       })
                     }
-                    
                   >
                     <option value="data1">データ 1</option>
                     <option value="data2">データ 2</option>
@@ -494,26 +527,24 @@ function App() {
                         }
                       />
                     </>
-                    
                   </HStack>
                   <>
-                      <Text>日付選択</Text>
-                      <Input
-                        placeholder="Select Date and Time"
-                        size="md"
-                        type="date"
-                        defaultValue='2019-01-01'
-                        max="2019-12-31"
-                        min="2019-01-01"
-                        onChange={(e) =>
-                          dispatch({
-                            type: "change_graph_option",
-                            field: "day",
-                            payload: (e.currentTarget.value),
-                          })
-                        }
-                      />
-                    </>
+                    <Text>日付選択</Text>
+                    <Input
+                      size="md"
+                      type="date"
+                      defaultValue={"2019-01-01"}
+                      max="2021-12-31"
+                      min="2021-01-01"
+                      onChange={(e) =>
+                        dispatch({
+                          type: "change_graph_option",
+                          field: "day",
+                          payload: e.currentTarget.value,
+                        })
+                      }
+                    />
+                  </>
                 </Box>
               </Box>
               <Box my={"1em"}>
@@ -526,21 +557,25 @@ function App() {
                 <Text>渋滞時:{state.calc_option.capacity_jutai}</Text>
               </>
               <>
-              <Text>日付:{state.graph_option.day}</Text>
+                <Text>日付:{state.graph_option.day}</Text>
               </>
             </GridItem>
             <GridItem>
-            <>
+              <>
                 <Text>グラフ描画</Text>
-                <Box my={"2em"}>
-                  <Chart
-                    chartType="ComboChart"
-                    width="100%"
-                    height="400px"
-                    data={graph_data}
-                    options={graph_options}
-                  />            
-                </Box>
+                {state.graph_data.length > 0 ? (
+                  <Box my={"2em"}>
+                    <Chart
+                      chartType="ComboChart"
+                      width="100%"
+                      height="400px"
+                      data={state.graph_data}
+                      options={graph_options}
+                    />
+                  </Box>
+                ) : (
+                  ""
+                )}
               </>
               <Box>
                 <Text>◆データ一覧（1時間ピッチ）</Text>
@@ -561,7 +596,6 @@ function App() {
                   ]}
                 ></ViewTable>
               </Box>
-
             </GridItem>
           </Grid>
         </Container>
